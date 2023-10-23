1e20baf3da3ea8189443bc0e3f867278525faabc
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-10-23 00:00:15 -0300

    Pagina implementada

--- BATERIA_DE_DESAFIOS_02/02-steps/src/App.jsx
+++ BATERIA_DE_DESAFIOS_02/02-steps/src/App.jsx
@@ -3,7 +3,34 @@ const App = () => {
 
   return (
     <>
-      <button>Fechar</button>
+      <div className="container-close">
+        <button className="close">Fechar</button>
+      </div>
+      <div className="steps">
+        <div className="numbers">
+          <div className="buttons active">
+            <span>1</span>
+          </div>
+          <div className="buttons">
+            <span>2</span>
+          </div>
+          <div className="buttons">
+            <span>3</span>
+          </div>
+          <div className="buttons">
+            <span>4</span>
+          </div>
+        </div>
+        <h2 className="message">
+          Passo 1: Entender o problema do cliente
+        </h2>
+        <div className="buttons">
+          <button>
+            Anterior
+          </button>
+          <button>Próximo</button>
+        </div>
+      </div>
     </>
   )
 }
