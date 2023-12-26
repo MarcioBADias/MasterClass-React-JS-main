f19c66d386e446b68d841d4d3bd32ae741323432
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-12-26 07:24:44 -0300

    ☑ Aprendendo mais sobre useEffect

--- BATERIA_DE_DESAFIOS_08/src/app.jsx
+++ BATERIA_DE_DESAFIOS_08/src/app.jsx
@@ -1,9 +1,11 @@
 const App = () => {
-  return (
-    <>
-      <h1>Boilerplace + React</h1>
-    </>
-  )
-}
+  const handleClick = () => {
+    fetch('./src/fake-user.json')
+      .then(r => r.json())
+      .then(console.log)
+      .catch(console.log)
+  }
 
+  return <button onClick={handleClick}>Buscar dados</button>
+}
 export { App }
