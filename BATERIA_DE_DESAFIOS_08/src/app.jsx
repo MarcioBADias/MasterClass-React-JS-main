00410884830ea72e51bac31e7f31a12537f1e252
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-12-26 08:19:21 -0300

    ⚠ Resolução de exercicio 1 (aguardando correção)

--- BATERIA_DE_DESAFIOS_08/src/app.jsx
+++ BATERIA_DE_DESAFIOS_08/src/app.jsx
@@ -1,11 +1,26 @@
-const App = () => {
-  const handleClick = () => {
-    fetch('./src/fake-user.json')
-      .then(r => r.json())
-      .then(console.log)
-      .catch(console.log)
-  }
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react"
 
-  return <button onClick={handleClick}>Buscar dados</button>
+const App = () => {
+  const [cats, setCats] = useState([])
+  const [catImg, setCatImg] = useState(null)
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/MarcioBADias/data-fake/main/ten-cats.json')
+      .then(res => res.json())
+      .then(data => setCats(data))
+      .catch(console.log)
+  }, [cats])
+
+  const handleClick = () => {
+    console.log(cats[0].url)
+    setCatImg(cats[Math.floor(Math.random() * 10)].url)
+  }
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+      {catImg !== null && <img src={catImg} style={{ maxWidth: 500 }} />}
+      <button onClick={handleClick} style={{ marginTop: '1rem' }}>Mostrar gatinhos</button>
+    </div>
+  )
 }
 export { App }
