e30c7c33aadf7aa4a36f3df377e182f6a5804142
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-01-08 08:47:10 -0300

    ✅Correção 2

--- BATERIA_DE_DESAFIOS_08/src/app.jsx
+++ BATERIA_DE_DESAFIOS_08/src/app.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 const App = () => {
   const [dog, setDog] = useState(null)
+  const [cats, setCats] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+      .then(r => r.json())
+      .then(data => setCats(data))
+      .catch(console.log)
+  }, [])
 
   const handleClick = () => {
     fetch('https://random.dog/woof.json').then(r => r.json()).then(data => setDog(data)).catch(console.log)
@@ -13,6 +21,19 @@ const App = () => {
     <>
       {dog?.url && <img src={dog.url} alt='Foto de cachorro aleatório' />}
       <button onClick={handleClick}>Mostrar um cachorro</button>
+
+      {cats.length > 0 &&
+        <ul>
+          {cats.map((img, i) => i < 3 && (
+            <li key={img.id}>
+              <img
+                style={{ maxWidth: 200, maxHeight: 200 }}
+                src={img.url}
+                alt={`Gato numero ${img.id}`} />
+            </li>
+          ))}
+        </ul>
+      }
     </>
   )
 }
