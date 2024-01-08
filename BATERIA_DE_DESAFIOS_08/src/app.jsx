d15ef2b47a163147969b635c1eade38a05d1047b
MarcioBADias <marcio.b.a.dias@gmail.com>
2024-01-08 08:00:05 -0300

    ✅ Correção 1

--- BATERIA_DE_DESAFIOS_08/src/app.jsx
+++ BATERIA_DE_DESAFIOS_08/src/app.jsx
@@ -1,38 +1,19 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from "react"
-
-const memesAPI = 'https://raw.githubusercontent.com/MarcioBADias/data-fake/main/memes.json'
-
-const getRandomMemes = (memes, count) => {
-  const shuffledMemes = memes.sort(() => 0.5 - Math.random())
-  return shuffledMemes.slice(0, count)
-}
+import React, { useState } from "react"
 
 const App = () => {
-  const [memes, setMemes] = useState([])
-  const [randomMemes, setRandomMemes] = useState([])
+  const [dog, setDog] = useState(null)
 
-  useEffect(() => {
-    fetch(memesAPI)
-      .then((res) => res.json())
-      .then((data) => setMemes(data))
-      .catch(console.log)
-  }, [])
-
-  useEffect(() => {
-    if (memes.length > 0) {
-      const randomMemes = getRandomMemes(memes, 3)
-      setRandomMemes(randomMemes)
-    }
-  }, [memes])
+  const handleClick = () => {
+    fetch('https://random.dog/woof.json').then(r => r.json()).then(data => setDog(data)).catch(console.log)
+  }
 
   return (
-    <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
-      {randomMemes.map((meme, index) => (
-        <img key={index} src={meme.url} style={{ maxWidth: 500, margin: "10px" }} />
-      ))}
-    </div>
+    <>
+      {dog?.url && <img src={dog.url} alt='Foto de cachorro aleatório' />}
+      <button onClick={handleClick}>Mostrar um cachorro</button>
+    </>
   )
 }
 
